845df0790756b7a969f39c390d0abbe2621c8c94
Binh Nguyen <jackjack2000.kahp@gmail.com>
2025-11-05 16:05:23 +0700

    add callback func

--- server/model/product/ProductModel.go
+++ server/model/product/ProductModel.go
@@ -2,6 +2,7 @@ package product;
 
 type Product struct {
   ID string;
+  Name string;
   Weight float32;
   X, Y, Z float32;
   Barcode Barcode;
@@ -13,4 +14,9 @@ func (product *Product) SetWeight(weight float32) {
 
 func (product *Product) GetVolume() float32 {
   return product.X * product.Y * product.Z;
+}
+
+func (product *Product) SampleCallback(name string, callback func(string)) {
+  product.Name = name + "_processed";
+  callback(product.Name);
 }
