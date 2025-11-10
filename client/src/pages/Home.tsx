d17bd83282ab009b107ab68542bd77279840f429
Binh Nguyen <jackjack2000.kahp@gmail.com>
2025-11-10 11:16:16 +0700

    update code

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,20 +1,6 @@
-import { useAsyncList } from "react-stately";
-
 import { DefaultLayout } from "@components";
 
 export default function Home() {
-  let list = useAsyncList({
-    async load({ signal }) {
-      let res = await fetch("https://swapi.py4e.com/api/people/?search", {
-        signal,
-      });
-      let json = await res.json();
-      return {
-        items: json.results,
-      };
-    },
-  });
-
   return (
     <DefaultLayout>
       <div className="space-y-4">
