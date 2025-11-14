944085721d2d19c961576e227b6986957c19440a
Binh Nguyen <jackjack2000.kahp@gmail.com>
2025-11-14 10:42:21 +0700

    remove footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,10 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full flex items-center justify-center py-3">
-      <div className="flex items-center gap-2">
-        <span className="text-default-600"> Footer </span>
-        <p className="text-primary"> Footer </p>
-      </div>
+    <footer className="w-full flex items-center justify-center">
     </footer>
   )
 }
